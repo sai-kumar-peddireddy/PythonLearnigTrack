41f83de125af31f2b04873197fdc1807a9b9a823
sai-kumar-peddireddy <sai.peddireddy77@gmail.com>
2018-07-09 08:20:38 +0530

    completed print function and partially completed math

--- Math/Math.py
+++ Math/Math.py
@@ -1,1 +1,49 @@
-#
+#09/07/2018
+'''
+Math related functions in python
+'''
+import math
+
+print('addition')
+print('2+3 = ',2+3)
+
+print('subtraction')
+print('2-3 = ',2-3)
+
+print('multiplication')
+print('2*3 = ',2*3)
+
+print('division')
+print('2/3 = ',2/3)
+
+print('modulus')
+print('2%3 = ',2%3)
+
+'''
+ceil(x, /)
+        Return the ceiling of x as an Integral.
+        
+        This is the smallest integer >= x.
+'''
+print(math.ceil(10.1)) #11
+
+print(math.ceil(-10.1)) #-10
+
+'''
+ copysign(x, y, /)
+        Return a float with the magnitude (absolute value) of x but the sign of y.
+        
+        On platforms that support signed zeros, copysign(1.0, -0.0)
+        returns -1.0.
+        it simply copies sign of the another number 
+'''
+
+print(math.copysign(10.5,1))
+
+'''
+exp(x, /)
+        Return e raised to the power of x.
+        value of exponent is e = 2.718
+'''
+
+print(math.exp(5))
--- PrintFunction/HelloWord.py
+++ PrintFunction/HelloWord.py
@@ -1,3 +1,58 @@
+# 7/9/2018
+
+#single line comment
+'''
+multi line comment
+'''
+#source of Learnig: sentdx youtube url :https://www.youtube.com/watch?v=UsCQXe1OHZk&list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M&index=3
+
+#print with ''
+
 print('I started  learning finally !!!!!!!!!!!')
 print('I am also learning git!!!!')
-print("Let's see how many days it will take :)" )
+print('line1\nline2') #newline
+print('every print() starts with a new line')
+print('line3')
+print('line4')
+print('print concatination')
+print('string1'+' string2')
+print('string1' ' string2')
+'''
+this gives an error 
+ print('Let's see how many days it will take me to learn python')
+ put \ to fix
+'''
+print('Let\'s see how many days it will take me to learn python')
+
+# printing with ""
+
+print("Hi" " how are you")
+print("hi \nhow are you")    #newline \n
+'''
+This gives an error 
+print("hello "world"")
+'''
+print('hello "world"')
+
+print("we use \n for newline") #but it's not going to print as expected
+
+print("we use '\\n' for newline") #method 1
+
+print("we use \\n for newline")
+
+print(r"we use \n for newline") #to print raw string
+'''
+print using numbers
+'''
+
+print('string and number',5)
+
+print('string',1)
+
+# this not going to work  print('string1' + 5)
+
+print('string1'+' 5')
+
+print(int('1') + 5)
+print(float('1.2') + 5)
+
