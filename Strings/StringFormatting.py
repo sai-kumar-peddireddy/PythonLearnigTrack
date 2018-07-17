04949d21883d7acaa62c5b5aa38331bdbdd31c77
sai-kumar-peddireddy <sai.peddireddy77@gmail.com>
2018-07-17 07:04:04 +0530

    completed stringformatting

--- Strings/StringFormatting.py
+++ Strings/StringFormatting.py
@@ -5,7 +5,7 @@ http://www.bogotobogo.com/python/python_string_formatting.php
 """
 floatval = 10.56
 print(floatval)
-string = "float variable in string as float %f,right justified:%8f, right justified starting with 0:%08.2f \
+string = "float variable in string as float %f,right justified:%8f, right justified starting with 0 :%08.2f \
 right justified with '+'sign :%+08.2f," % (floatval, floatval, floatval, floatval)
 
 print(string)
@@ -41,4 +41,35 @@ print(string)
 string = "intval variable in string as Exponent %E" % intval
 
 print(string)
+
+# Tue Jul 17 05:07:48 IST 2018
+
+# Formatting dictionary strings
+
+dic_string = ""
+dic = {'key1': 10, 'key2': "value"}
+
+string = "from the dic %(key1)d, %(key2)s" % dic
+print(string)
+string = "string is filling now %(key1)d, %(key2)s"
+
+print(string % dic)
+
+# Template basics
+
+# By position
+t = '{0}, {1}, {2}'
+
+print(t.format('17', 'July', '2018'))
+
+# By keyword
+t = '{Day}, {Month}, {Year}'
+
+print(t.format(Day='17', Month='July', Year='2018'))
+
+# By Mixed
+
+t = '{Month}, {Year}, {0}'
+
+print(t.format('17', Month='July', Year='2018'))
 
