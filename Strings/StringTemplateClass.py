166dd30311981a122a85ce3abfc6766fc0d4266a
sai-kumar-peddireddy <sai.peddireddy77@gmail.com>
2018-07-18 06:21:49 +0530

    StringTemplate

--- Strings/StringTemplateClass.py
+++ Strings/StringTemplateClass.py
@@ -1,3 +1,18 @@
 """
 Wed Jul 18 05:40:41 IST 2018
+source :-https://www.geeksforgeeks.org/template-class-in-python/
+Template Class allows us to create simplified syntax for output specification.
+The format uses placeholder names formed by $ with valid Python identifiers
+in StringTemplate we will create a generalised output specification then we will use that at requred generalised form
+
 """
+from string import Template
+
+names_list = ['Sai kumar', 'Nagesh', 'Durga', 'RGV']
+# creating a template with placeholder
+
+t = Template('Hi Mr.$name ')  # $name is a placeholder
+for i in names_list:
+    print(t.substitute(name=i))
+
+
